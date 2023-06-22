e8d5bc6ff1936277b4a096ad000621f0924b0bdd
Freddy JUHEL <ext.freddy.juhel@reseau.sncf.fr>
2023-06-22 15:04:26 +0200

    Add get architecture method

--- Source/WinForm.cs
+++ Source/WinForm.cs
@@ -7,3 +7,7 @@ public static string DisplayTitle()
   return $" V{fvi.FileMajorPart}.{fvi.FileMinorPart}.{fvi.FileBuildPart}.{fvi.FilePrivatePart}"; // doesn't build in appveyor
 }
 
+public static string GetArchitecture()
+  {
+    Return Environment.Is64BitOperatingSystem ? "X64" : "X86";
+  }
